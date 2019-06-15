8c0954c5bd2081f0d6cd3c95968e7991aab9ea80
Andres Orav <andres.orav@pipedrive.com>
2019-06-15 13:50:18 +0300

    Add cuisine to IRestaurant interface

--- AbstractFactoryPattern/Factories/IRestaurant.ts
+++ AbstractFactoryPattern/Factories/IRestaurant.ts
@@ -3,6 +3,7 @@ import { IMenuItem } from '../Products/IMenuItem';
 export interface IRestaurant {
 	readonly name: string,
 	readonly currency: string,
+	readonly cuisine: string;
 
 	getBreakfastMenu(): IMenuItem[],
 	getLunchMenu(): IMenuItem[],
