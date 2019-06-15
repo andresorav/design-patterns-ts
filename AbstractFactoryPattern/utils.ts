42e568497762c400d9fdf780e568347783f4315e
Andres Orav <andres.orav@pipedrive.com>
2019-06-15 15:02:08 +0300

    Now menu items are grouped by type

--- AbstractFactoryPattern/utils.ts
+++ AbstractFactoryPattern/utils.ts
@@ -2,18 +2,32 @@ import { IRestaurant } from './Factories/IRestaurant';
 import { IMenuItem } from './Products/IMenuItem';
 
 const printMenuItems = (menuItems: IMenuItem[], currency: string) => {
-	menuItems.map((menuItem) => {
-		console.log(`- ${menuItem.name}: ${menuItem.price} ${currency}`);
+	const groupedMenuItems = menuItems.reduce(function(menuItems, current) {
+		(menuItems[current.type] = menuItems[current.type] || []).push(current);
+
+		return menuItems;
+	}, {});
+
+	Object.keys(groupedMenuItems).map((menuItemType) => {
+		console.log(`~ ${menuItemType} ~`);
+
+		groupedMenuItems[menuItemType].map((menuItem) => {
+			console.log(`- ${menuItem.name}: ${menuItem.price} ${currency}`);
+		});
+
+		console.log();
 	});
 };
 
 const printMenuItemsTotalPrice = (menuItems: IMenuItem[], currency: string) => {
 	const priceForItems = menuItems.reduce((sum, current) => sum + current.price, 0);
+
+	console.log(Array(14).join('_'));
 	console.log(`Total: ${priceForItems} ${currency}`);
 };
 
 const printRestaurantMenus = (restaurant: IRestaurant) => {
-	console.log(`\n~~~ ${restaurant.name} (${restaurant.cuisine}) ~~~`);
+	console.log(`\n~~~ ${restaurant.name} (${restaurant.cuisine} restaurant) ~~~`);
 
 	const menus = [{
 		name: 'Breakfast',
