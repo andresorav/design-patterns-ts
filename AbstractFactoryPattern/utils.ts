b206552173fd7ff79d6a82127f4887a610ae0b89
Andres Orav <andres.orav@pipedrive.com>
2019-06-22 14:16:32 +0300

    Fix eslint warnings

--- AbstractFactoryPattern/utils.ts
+++ AbstractFactoryPattern/utils.ts
@@ -1,57 +1,69 @@
 import { IRestaurant } from './Factories/IRestaurant';
 import { IMenuItem } from './Products/IMenuItem';
 
-const printMenuItems = (menuItems: IMenuItem[], currency: string) => {
-	const groupedMenuItems = menuItems.reduce(function(menuItems, current) {
-		(menuItems[current.type] = menuItems[current.type] || []).push(current);
+interface GroupedMenuItems {
+    [type: string]: IMenuItem[];
+}
 
-		return menuItems;
-	}, {});
+const groupMenuItemsByType = (menuItems: IMenuItem[]): GroupedMenuItems => {
+    return menuItems.reduce((menuItems: GroupedMenuItems, current: IMenuItem): GroupedMenuItems => {
+        if (!menuItems[current.type]) {
+            menuItems[current.type] = [];
+        }
 
-	Object.keys(groupedMenuItems).map((menuItemType) => {
-		console.log(`~ ${menuItemType} ~`);
+        menuItems[current.type].push(current);
 
-		groupedMenuItems[menuItemType].map((menuItem) => {
-			console.log(`- ${menuItem.name}: ${menuItem.price} ${currency}`);
-		});
-
-		console.log();
-	});
+        return menuItems;
+    }, {});
 };
 
-const printMenuItemsTotalPrice = (menuItems: IMenuItem[], currency: string) => {
-	const priceForItems = menuItems.reduce((sum, current) => sum + current.price, 0);
-	const total = `Total price: ${priceForItems} ${currency}`;
+const printMenuItems = (menuItems: IMenuItem[], currency: string): void => {
+    const groupedMenuItems = groupMenuItemsByType(menuItems);
 
-	console.log(Array(total.length + 1).join('_'));
-	console.log(total);
+    Object.keys(groupedMenuItems).forEach((menuItemType: string): void => {
+        console.log(`~ ${menuItemType} ~`);
+
+        groupedMenuItems[menuItemType].forEach((menuItem: IMenuItem): void => {
+            console.log(`- ${menuItem.name}: ${menuItem.price} ${currency}`);
+        });
+
+        console.log();
+    });
 };
 
-const printRestaurantMenus = (restaurant: IRestaurant) => {
-	console.log(`\n~~~ ${restaurant.name} (${restaurant.cuisine} restaurant) ~~~`);
+const printMenuItemsTotalPrice = (menuItems: IMenuItem[], currency: string): void => {
+    const priceForItems = menuItems.reduce((sum, current): number => sum + current.price, 0);
+    const total = `Total price for menu: ${priceForItems} ${currency}`;
 
-	const menus = [{
-		name: 'Breakfast',
-		items: restaurant.getBreakfastMenu(),
-	}, {
-		name: 'Lunch',
-		items: restaurant.getLunchMenu(),
-	}, {
-		name: 'Dinner',
-		items: restaurant.getDinnerMenu(),
-	}, {
-		name: 'Drinks',
-		items: restaurant.getDrinks(),
-	}];
+    console.log(Array(total.length + 1).join('_'));
+    console.log(total);
+};
 
-	menus.map((menu) => {
-		console.log(`\n*** ${menu.name} ***`);
+const printRestaurantMenus = (restaurant: IRestaurant): void => {
+    console.log(`\n~~~ ${restaurant.name} (${restaurant.cuisine} restaurant) ~~~`);
 
-		printMenuItems(menu.items, restaurant.currency);
-		printMenuItemsTotalPrice(menu.items, restaurant.currency);
-	});
+    const menus = [{
+        name: 'Breakfast',
+        items: restaurant.getBreakfastMenu(),
+    }, {
+        name: 'Lunch',
+        items: restaurant.getLunchMenu(),
+    }, {
+        name: 'Dinner',
+        items: restaurant.getDinnerMenu(),
+    }, {
+        name: 'Drinks',
+        items: restaurant.getDrinks(),
+    }];
+
+    menus.forEach((menu): void => {
+        console.log(`\n*** ${menu.name} ***`);
+
+        printMenuItems(menu.items, restaurant.currency);
+        printMenuItemsTotalPrice(menu.items, restaurant.currency);
+    });
 };
 
 export {
-	printRestaurantMenus,
+    printRestaurantMenus,
 }
