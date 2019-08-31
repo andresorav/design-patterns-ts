dc7bf302df069387d08c1da504eb8ba6b0a4ab86
Andres Orav <andres.orav@pipedrive.com>
2019-08-31 16:28:42 +0300

    Add bridge pattern to main project and update scripts

--- src/designPatterns.ts
+++ src/designPatterns.ts
@@ -1,5 +1,7 @@
 import { patternExample as abstractFactoryPattern } from './abstractFactoryPattern/abstractFactoryPattern';
 import { patternExample as adapterPattern } from './adapterPattern/adapterPattern';
+import { patternExample as bridgePattern } from './bridgePattern/bridgePattern';
 
 abstractFactoryPattern();
 adapterPattern();
+bridgePattern();
